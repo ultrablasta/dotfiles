beac43a805cd5fa2f8b5275bd9d954478202cc02
ultrablasta <ultrablasta@protonmail.com>
2021-02-16 14:13:01 +1100

    Added fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,4 @@
+abbr ytdl "youtube-dl --add-metadata -ico '[%(channel)s] %(upload_date)s - %(title)s.%(ext)s'"
+abbr rbackup "sudo rsync --progress --delete -naAXv"
+abbr ddwipe "sudo dd if=/dev/urandom of=/dev/sdX status=progress bs=4096"
+
